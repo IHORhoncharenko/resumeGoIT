478d5fa3d9499b09a400eb7728dbb3219ad15ec4
Ihor Honcharenko <89031570+IHORhoncharenko@users.noreply.github.com>
2023-08-27 15:59:59 +0300

    Add files via upload

--- resume-project/script.js
+++ resume-project/script.js
@@ -7,16 +7,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
   [...document.getElementsByClassName('p-contact--info')].forEach((e) => {
     (e.textContent.includes('C')) && (`
-      ${e.children[0].style.color = '#161d2a'}
-      ${e.children[0].style.fontFamily = 'Montserrat'}
+      ${e.children[0].style.color = '#161D2A'}
       ${e.children[0].style.fontSize = '14px'}
       ${e.children[0].style.fontStyle = 'normal'}
       ${e.children[0].style.fontWeight = '400'}
       ${e.children[0].style.lineHeight = '24px'}
+      ${e.children[0].style.textDecorationLine = 'none'}
     `);
     (e.textContent.includes('E')) && (`
-      ${e.children[0].style.color = '#2979ff'}
-      ${e.children[0].style.fontFamily = 'Montserrat'}
+      ${e.children[0].style.color = '#2979FF'}
       ${e.children[0].style.fontSize = '14px'}
       ${e.children[0].style.fontStyle = 'normal'}
       ${e.children[0].style.fontWeight = '400'}
@@ -31,10 +30,10 @@ document.addEventListener('DOMContentLoaded', () => {
         li.children[0].insertAdjacentHTML('afterend', `${'.'.repeat(100)}`);
         break;
       case 1:
-        li.children[0].insertAdjacentHTML('afterend', `${'.'.repeat(57)}`);
+        li.children[0].insertAdjacentHTML('afterend', `${'.'.repeat(42)}`);
         break;
       case 2:
-        li.children[0].insertAdjacentHTML('afterend', `${'.'.repeat(103)}`);
+        li.children[0].insertAdjacentHTML('afterend', `${'.'.repeat(104)}`);
         break;
       default:
         break;
